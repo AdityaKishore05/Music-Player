52124c2e2d31559667727d9e075ae34230508bcf
aditya <ayuadi2845@gmail.com>
2025-11-22 00:58:24 +0530

    Merge remote and local histories

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import "./globals.css";
-<<<<<<< HEAD
-=======
 import { PlayerProvider } from "./music/@context/PlayerContext"; // adjust if needed
->>>>>>> 81ef279003a6e74585b2a69c911e3ed17ffd32d5
 
 export default function RootLayout({
   children,
@@ -12,11 +9,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-<<<<<<< HEAD
-        {children}
-=======
         <PlayerProvider>{children}</PlayerProvider>
->>>>>>> 81ef279003a6e74585b2a69c911e3ed17ffd32d5
       </body>
     </html>
   );
